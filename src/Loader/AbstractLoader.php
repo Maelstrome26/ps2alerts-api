6f4014166ed8e825e37d92221d335dfdd0ce9612
Maelstrome26 <maelstrome26@gmail.com>
2015-12-14 21:10:56 +0000

    Docblocks

--- src/Loader/AbstractLoader.php
+++ src/Loader/AbstractLoader.php
@@ -9,26 +9,53 @@ abstract class AbstractLoader implements RedisAwareInterface
 {
     use RedisAwareTrait;
 
+    /**
+     * Checks for a key within Redis and returns it's existance
+     *
+     * @param  string  $key
+     * @return boolean
+     */
     public function checkRedis($key)
     {
         return $this->getRedisDriver()->exists($key);
     }
 
+    /**
+     * Retrieves a key from within Redis
+     *
+     * @param  string $key
+     * @return string JSON decoded value
+     */
     public function getFromRedis($key)
     {
         return json_decode($this->getRedisDriver()->get($key));
     }
 
-    public function setExpireKey($key, $value, $secs = 3600)
+    /**
+     * Sets JSON encoded value within Redis with an expiry in seconds
+     *
+     * @param string  $key
+     * @param mixed   $value   Data to encode into JSON and store
+     * @param integer $expires Cache expiry time in seconds
+     */
+    public function setExpireKey($key, $value, $expires = 3600)
     {
         $value = json_encode($value);
-        return $this->getRedisDriver()->setEx($key, $secs, $value);
+        return $this->getRedisDriver()->setEx($key, $expires, $value);
     }
 
-    public function cacheAndReturn($result, $key)
+    /**
+     * Caches the data gathered and then returns again to the source script
+     *
+     * @param  mixed  $data Data to store
+     * @param  string $key  Redis key to be used to store the data
+     *
+     * @return mixed  $data
+     */
+    public function cacheAndReturn($data, $key)
     {
-        $this->setExpireKey($key, $result);
-        $result['cached'] = 0;
-        return $result;
+        $this->setExpireKey($key, $data);
+        $data['cached'] = 0;
+        return $data;
     }
 }
