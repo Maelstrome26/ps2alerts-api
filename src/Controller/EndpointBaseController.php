38c547e777e5c789f6c11e66eb4904e05227df79
Maelstrome26 <maelstrome26@gmail.com>
2015-12-17 19:48:25 +0000

    Added ability to set entire request as cacheable or not

--- src/Controller/EndpointBaseController.php
+++ src/Controller/EndpointBaseController.php
@@ -6,7 +6,31 @@ use Symfony\Component\HttpFoundation\Request;
 use League\Route\Http\JsonResponse as Response;
 use Ps2alerts\Api\QueryObjects\QueryObject;
 
-class EndpointBaseController
+abstract class EndpointBaseController
 {
     protected $repository;
+
+    protected $cacheable = true;
+
+    protected $cacheNamespace;
+
+    /**
+     * Sets a flag whether or not the content should be cached
+     *
+     * @param boolean $toggle
+     */
+    public function setCacheable($toggle)
+    {
+        // If statement is here because can't figure out how to typehint a boolean...
+        if ($toggle === true || $toggle === false) {
+            $this->cacheable = $toggle;
+        }
+    }
+
+    /**
+     * Require that the endpoints define a cache key namespace
+     */
+    abstract public function setCacheNamespace();
+
+    abstract public function readSingle(Request $request, array $args);
 }
