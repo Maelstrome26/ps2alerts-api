c6f6c5472abf66ea69cacd436ab865a72669fcfc
Maelstrome26 <maelstrome26@gmail.com>
2015-12-19 21:32:31 +0000

    Implemented Top 10 Outfits Statistics endpoint

--- src/Loader/Statistics/OutfitTotalsMetricsLoader.php
+++ src/Loader/Statistics/OutfitTotalsMetricsLoader.php
@@ -3,6 +3,7 @@
 namespace Ps2alerts\Api\Loader\Statistics;
 
 use Ps2alerts\Api\Loader\Statistics\AbstractStatisticsLoader;
+use Ps2alerts\Api\QueryObjects\QueryObject;
 use Ps2alerts\Api\Repository\Statistics\OutfitTotalsRepository;
 
 class OutfitTotalsMetricsLoader extends AbstractStatisticsLoader
@@ -24,16 +25,37 @@ class OutfitTotalsMetricsLoader extends AbstractStatisticsLoader
     }
 
     /**
-     * Pulls the top X of the statistic
-     *
-     * @param  integer $length Number of top items to return
+     * Returns the top X of a particular statistic
      *
      * @return array
      */
-    public function readTop($length = 10)
+    public function readTop($limit = 10, $metric = 'outfitKills')
     {
-        $this->setType("OutfitTotals:Top{$length}");
+        $redisKey = "{$this->getCacheNamespace()}:{$this->getType()}:{$metric}:{$limit}";
 
-        
+        // Enforce a max limit
+        if ($limit > 50) {
+            $limit = 50;
+        }
+
+        if ($this->checkRedis($redisKey)) {
+            return $this->getFromRedis($redisKey);
+        }
+
+        $queryObject = new QueryObject;
+        // Prevent the VS, NC and TR "no outfit" workaround
+        $queryObject->addWhere([
+            'col' => 'outfitID',
+            'op' => '>',
+            'value' => 0
+        ]);
+        $queryObject->setOrderBy($metric);
+        $queryObject->setOrderByDirection('desc');
+        $queryObject->setLimit($limit);
+
+        return $this->cacheAndReturn(
+            $this->repository->read($queryObject),
+            $redisKey
+        );
     }
 }
