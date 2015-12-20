709d1bff9073cd1e77e0bf651a8c2c5df9dce517
Maelstrome26 <maelstrome26@gmail.com>
2015-12-20 17:21:01 +0000

    Removed obsolete hookup code in Container, since it's all handled by Use statements in each class.

--- src/container.php
+++ src/container.php
@@ -22,35 +22,7 @@ $container->inflector('Ps2alerts\Api\Contract\TemplateAwareInterface')
           ->invokeMethod('setTemplateDriver', ['Twig_Environment']);
 $container->inflector('Ps2alerts\Api\Contract\RedisAwareInterface')
           ->invokeMethod('setRedisDriver', ['redis']);
-
-// Repositories
-$container->add('Ps2alerts\Api\Repository\ResultRepository');
-// Metrics Repositories
-$container->add('Ps2alerts\Api\Repository\Metrics\MapRepository');
-$container->add('Ps2alerts\Api\Repository\Metrics\MapInitialRepository');
-$container->add('Ps2alerts\Api\Repository\Metrics\OutfitRepository');
-
-// Loaders
-$container->add('Ps2alerts\Api\Loader\Metrics\MapMetricsLoader')
-          ->withArgument('Ps2alerts\Api\Repository\Metrics\MapRepository');
-$container->add('Ps2alerts\Api\Loader\Metrics\MapInitialMetricsLoader')
-          ->withArgument('Ps2alerts\Api\Repository\Metrics\MapInitialRepository');
-$container->add('Ps2alerts\Api\Loader\Metrics\OutfitMetricsLoader')
-          ->withArgument('Ps2alerts\Api\Repository\Metrics\OutfitRepository');
-$container->add('Ps2alerts\Api\Loader\ResultLoader')
-          ->withArgument('Ps2alerts\Api\Repository\ResultRepository');
-
-// Endpoint Injectors
-$container->add('Ps2alerts\Api\Controller\Alerts\ResultsEndpointController')
-          ->withArgument('Ps2alerts\Api\Loader\ResultLoader');
-// Metrics Endpoints
-$container->add('Ps2alerts\Api\Controller\Metrics\MapMetricsEndpoint')
-          ->withArgument('Ps2alerts\Api\Loader\Metrics\MapMetricsLoader');
-$container->add('Ps2alerts\Api\Controller\Metrics\MapInitialMetricsEndpoint')
-          ->withArgument('Ps2alerts\Api\Loader\Metrics\MapInitialMetricsLoader');
-$container->add('Ps2alerts\Api\Controller\Metrics\OutfitMetricsEndpoint')
-          ->withArgument('Ps2alerts\Api\Loader\Metrics\OutfitMetricsLoader');
-
+          
 // Container Inflector
 $container->inflector('League\Container\ContainerAwareInterface')
           ->invokeMethod('setContainer', [$container]);
