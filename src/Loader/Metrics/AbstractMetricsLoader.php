714d8a26e374590cbe06f6c3b2e187019bce8b1c
Maelstrome26 <maelstrome26@gmail.com>
2015-12-20 16:03:46 +0000

    Added ability for the metrics to be able to filter particular fields

--- src/Loader/Metrics/AbstractMetricsLoader.php
+++ src/Loader/Metrics/AbstractMetricsLoader.php
@@ -8,6 +8,13 @@ use Ps2alerts\Api\QueryObjects\QueryObject;
 abstract class AbstractMetricsLoader extends AbstractLoader
 {
     /**
+     * Allows injection of where statements
+     *
+     * @var array
+     */
+    protected $metrics = [];
+
+    /**
      * Returns metrics for a particular result
      *
      * @param string $id
@@ -28,9 +35,42 @@ abstract class AbstractMetricsLoader extends AbstractLoader
             'value' => $id
         ]);
 
+        if (! empty($this->getMetrics())) {
+            foreach ($this->getMetrics() as $metric) {
+                $hash = md5($metric['col'] . $metric['value']);
+                $redisKey .= ":{$hash}";
+
+                $queryObject->addWhere([
+                    'col'   => $metric['col'],
+                    'op'    => (isset($metric['op']) ? $metric['op'] : '='),
+                    'value' => $metric['value']
+                ]);
+            }
+        }
+
         return $this->cacheAndReturn(
             $this->repository->read($queryObject),
             $redisKey
         );
+    }
+
+    /**
+     * Allows setting of metrics to filter
+     *
+     * @param array
+     */
+    public function setMetrics($metrics)
+    {
+        $this->metrics[] = $metrics;
+    }
+
+    /**
+     * Pulls in metrics
+     *
+     * @return array
+     */
+    public function getMetrics()
+    {
+        return $this->metrics;
     }
 }
