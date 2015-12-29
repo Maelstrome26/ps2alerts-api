5f90894c898c269f0b4a9926694e6df5b9be5aa1
Maelstrome26 <maelstrome26@gmail.com>
2015-12-29 15:43:03 +0000

    Piss off CORS

--- public/index.php
+++ public/index.php
@@ -16,6 +16,13 @@ $container = include __DIR__ . '/../src/container.php';
 // Routes
 $router = include __DIR__ . '/../src/routes.php';
 
+// Add headers to the response object so CORS can suck a big fat dick
+$response = $container->get('Symfony\Component\HttpFoundation\Response');
+$response->headers->set('Access-Control-Allow-Origin', '*');
+$response->headers->set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+$response->headers->set('Access-Control-Max-Age', '1000');
+$response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
+
 // FIRE!!!
 try {
     $response = $router->dispatch(
@@ -23,9 +30,7 @@ try {
         $container->get('Symfony\Component\HttpFoundation\Request')->getPathInfo()
     );
 } catch (NotFoundException $e) {
-    $response = $container->get('Symfony\Component\HttpFoundation\Response');
-
-    $response->setContent(
+    $response->setResponseCode(404)->setContent(
         $container->get('Twig_Environment')->render('404.html')
     );
 }
