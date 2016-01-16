4755010cc63d753d5b8d32284aa1822ab5bb6ce8
Matthew Cavanagh <maelstrome26@gmail.com>
2016-01-16 18:35:25 +0000

    Added OutfitTransformer

---
+++ src/Transformer/Metrics/OutfitTransformer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ps2alerts\Api\Transformer\Metrics;
+
+use League\Fractal\TransformerAbstract;
+
+class OutfitTransformer extends TransformerAbstract
+{
+    /**
+     * The tranform method required by Fractal to parse the data and return proper typing and fields.
+     *
+     * @param  array $data Data to transform
+     *
+     * @return array
+     */
+    public function transform($data)
+    {
+        return [
+            'outfit'  => [
+                'id'      => (int) $data['outfitID'],
+                'name'    => (string) $data['outfitName'],
+                'tag'     => (string) $data['outfitTag'],
+                'faction' => (int) $data['outfitFaction']
+            ],
+            'metrics' => [
+                'kills'     => (int) $data['outfitKills'],
+                'deaths'    => (int) $data['outfitDeaths'],
+                'teamkills' => (int) $data['outfitTKs'],
+                'suicides'  => (int) $data['outfitSuicides'],
+                'captures'  => (int) $data['outfitCaps'],
+            ]
+        ];
+    }
+}
