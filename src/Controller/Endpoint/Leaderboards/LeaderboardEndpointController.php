9bff2b1c0384d94de566ab0fcdf5fb6e594ea182
Maelstrome26 <maelstrome26@gmail.com>
2016-02-22 13:25:21 +0000

    Added base LeaderboardEndpointController

---
+++ src/Controller/Endpoint/Leaderboards/LeaderboardEndpointController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Ps2alerts\Api\Controller\Endpoint\Leaderboards;
+
+use League\Fractal\Manager;
+use Ps2alerts\Api\Controller\Endpoint\AbstractEndpointController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LeaderboardEndpointController extends AbstractEndpointController
+{
+    /**
+     * Construct
+     *
+     * @param League\Fractal\Manager                     $fractal
+     */
+    public function __construct(
+        Manager $fractal
+    ) {
+        $this->fractal = $fractal;
+    }
+}
