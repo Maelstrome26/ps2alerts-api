855b487f726f6e38517e029402a68db59a63e87a
Matt Cavanagh <maelstrome26@gmail.com>
2016-04-28 23:10:05 +0100

    Added captures include to outfit endpoint

--- src/Transformer/Profiles/OutfitProfileTransformer.php
+++ src/Transformer/Profiles/OutfitProfileTransformer.php
@@ -3,10 +3,13 @@
 namespace Ps2alerts\Api\Transformer\Profiles;
 
 use League\Fractal\TransformerAbstract;
+use Ps2alerts\Api\Contract\DatabaseAwareInterface;
+use Ps2alerts\Api\Contract\DatabaseAwareTrait;
 use Ps2alerts\Api\Contract\HttpClientAwareInterface;
 use Ps2alerts\Api\Contract\HttpClientAwareTrait;
 use Ps2alerts\Api\Contract\RedisAwareInterface;
 use Ps2alerts\Api\Contract\RedisAwareTrait;
+use Ps2alerts\Api\Repository\Metrics\MapRepository;
 use Ps2alerts\Api\Repository\Metrics\OutfitRepository;
 use Ps2alerts\Api\Repository\Metrics\OutfitTotalRepository;
 use Ps2alerts\Api\Repository\Metrics\PlayerRepository;
@@ -15,8 +18,9 @@ use Ps2alerts\Api\Transformer\Profiles\OutfitInvolvementTransformer;
 use Ps2alerts\Api\Transformer\Profiles\OutfitPlayersTransformer;
 
 class OutfitProfileTransformer extends TransformerAbstract
-    implements HttpClientAwareInterface, RedisAwareInterface
+    implements DatabaseAwareInterface, HttpClientAwareInterface, RedisAwareInterface
 {
+    use DatabaseAwareTrait;
     use HttpClientAwareTrait;
     use RedisAwareTrait;
 
@@ -26,22 +30,26 @@ class OutfitProfileTransformer extends TransformerAbstract
      * @var array
      */
     protected $availableIncludes = [
+        'captures',
         'involvement',
         'metrics',
         'players'
     ];
 
+    protected $mapRepo;
     protected $outfitRepo;
     protected $outfitTotalRepo;
     protected $playerRepo;
     protected $playerTotalRepo;
 
     public function __construct(
+        MapRepository         $mapRepo,
         OutfitRepository      $outfitRepo,
         OutfitTotalRepository $outfitTotalRepo,
         PlayerRepository      $playerRepo,
         PlayerTotalRepository $playerTotalRepo
     ) {
+        $this->mapRepo         = $mapRepo;
         $this->outfitRepo      = $outfitRepo;
         $this->outfitTotalRepo = $outfitTotalRepo;
         $this->playerRepo      = $playerRepo;
@@ -71,43 +79,54 @@ class OutfitProfileTransformer extends TransformerAbstract
         ];
     }
 
-    public function getOutfitMembers($data, $offset)
+    public function includeCaptures($data)
     {
-        $client = $this->getHttpClientDriver();
         $redis = $this->getRedisDriver();
+        $key = "ps2alerts:api:profiles:outfit:captures:{$data['outfitID']}";
 
-        $key = "ps2alerts:api:profiles:outfit:census:members:{$data['outfitID']}:{$offset}";
+        if ($redis->exists($key)) {
+            $bases = json_decode($redis->get($key), true);
+        } else {
+            $bases = $this->getCaptureStats($data);
 
-        // If we have this cached already
-        if (! empty($redis->exists($key))) {
-            return json_decode($redis->get($key));
+            if (count($bases) > 0) {
+                $redis->setEx($key, 3600, json_encode($bases));
+            }
         }
 
-        $namespace = 'ps2:v2';
+        return $this->collection($bases, new OutfitCaptureTransformer);
+    }
 
-        if ($data['outfitServer'] >= 2000) {
-            $namespace = 'ps2ps4eu';
-        } elseif ($data['outfitServer'] >= 1000) {
-            $namespace = 'ps2ps4us';
+    public function getCaptureStats($data)
+    {
+        // For some reason, auraSQL or my abstraction code won't handle this. Time to do it manually.
+
+        $pdo = $this->getDatabaseDriver();
+
+        $statement = $pdo->prepare("SELECT defence, facilityID FROM ws_map WHERE outfitCaptured = ?");
+        $statement->bindValue(1, $data['outfitID']);
+        $statement->execute();
+
+        $bases = [];
+
+        while ($row = $statement->fetch(\PDO::FETCH_OBJ)) {
+            if (empty($bases[$row->facilityID])) {
+                $bases[$row->facilityID] = [
+                    'facility' => $row->facilityID,
+                    'captures' => 0,
+                    'defences' => 0
+                ];
+            }
+
+            if ($row->defence === '1') {
+                $bases[$row->facilityID]['defences']++;
+
+            } else {
+                $bases[$row->facilityID]['captures']++;
+            }
         }
 
-        $response = $client->get(
-            "https://census.daybreakgames.com/s:planetside2alertstats/get/{$namespace}/outfit_member?outfit_id={$data['outfitID']}&c:limit=5000&c:start={$offset}"
-        );
-
-        $body = json_decode($response->getBody()->getContents(), true);
-
-        $count = count($body['outfit_member_list']);
-        $chars = [];
-
-        for ($i = 0; $i < $count; $i++) {
-            $chars[] = $body['outfit_member_list'][$i]['character_id'];
-        }
-
-        // Cache results in redis
-        $redis->setEx($key, 3600, json_encode($chars));
-
-        return $chars;
+        return $bases;
     }
 
     public function includeInvolvement($data)
@@ -196,5 +215,44 @@ class OutfitProfileTransformer extends TransformerAbstract
         $redis->setEx($key, 3600, json_encode($players));
 
         return $players;
+    }
+
+    public function getOutfitMembers($data, $offset)
+    {
+        $client = $this->getHttpClientDriver();
+        $redis = $this->getRedisDriver();
+
+        $key = "ps2alerts:api:profiles:outfit:census:members:{$data['outfitID']}:{$offset}";
+
+        // If we have this cached already
+        if (! empty($redis->exists($key))) {
+            return json_decode($redis->get($key));
+        }
+
+        $namespace = 'ps2:v2';
+
+        if ($data['outfitServer'] >= 2000) {
+            $namespace = 'ps2ps4eu';
+        } elseif ($data['outfitServer'] >= 1000) {
+            $namespace = 'ps2ps4us';
+        }
+
+        $response = $client->get(
+            "https://census.daybreakgames.com/s:planetside2alertstats/get/{$namespace}/outfit_member?outfit_id={$data['outfitID']}&c:limit=5000&c:start={$offset}"
+        );
+
+        $body = json_decode($response->getBody()->getContents(), true);
+
+        $count = count($body['outfit_member_list']);
+        $chars = [];
+
+        for ($i = 0; $i < $count; $i++) {
+            $chars[] = $body['outfit_member_list'][$i]['character_id'];
+        }
+
+        // Cache results in redis
+        $redis->setEx($key, 3600, json_encode($chars));
+
+        return $chars;
     }
 }
